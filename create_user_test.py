b0b81dde5f95192d4efbdd37541d0d875f4e04ef
dimok- <dimok-@list.ru>
2023-11-02 14:21:14 +0400

    My first commit

---
+++ create_user_test.py
@@ -0,0 +1,14 @@
+import sender_stand_request
+import data
+
+# эта функция меняет значения в параметре firstName
+def test_create_user_2_letter_in_first_name_get_success_response():
+    # В переменную user_body сохраняется обновленное тело запроса с именем “Аа”
+    user_body = get_user_body("Aa")
+    # В переменную user_response сохраняется результат запроса на создание пользователя
+    user_response = sender_stand_request.post_new_user(user_body)
+
+    # Проверяется, что код ответа равен 201
+    assert user_response.status_code == 201
+    # Проверяется, что в ответе есть поле authToken, и оно не пустое
+    assert user_response.json()["authToken"] != ""
